1df09dc74a03849bbbd6a3574c46fcb86b3bb084
tokorom <tokorom@gmail.com>
2014-05-15 16:38:03 +0900

    Add versioning setting

--- XcodeVersioningSample/ViewController.m
+++ XcodeVersioningSample/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak) IBOutlet UILabel *label;
 @end
 
 @implementation ViewController
@@ -17,13 +17,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+    self.label.text = appVersion;
 }
 
 @end
